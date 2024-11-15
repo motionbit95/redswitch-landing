779ee794dfa9813d62211a01b04628339e0a9318
wlsdud9973 <jyp260399@gmail.com>
2024-11-15 16:20:45 +0900

    사파리 vh 적용

--- src/layout/page.js
+++ src/layout/page.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Pagination from "./pagination";
 
 function Page(props) {
+  useEffect(() => {
+    // 화면 크기를 계산하여 --vh 값을 설정하는 함수
+    function setVhVariable() {
+      const vh = window.innerHeight * 0.01; // 1vh의 크기를 계산
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+
+    // 초기 설정과 화면 크기 변경 시마다 --vh 값 업데이트
+    setVhVariable();
+    window.addEventListener("resize", setVhVariable);
+
+    // cleanup 함수에서 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("resize", setVhVariable);
+    };
+  }, []);
+
   return (
     <div className="page">
       {props.children}
